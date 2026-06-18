2991910bd7724a1d4fbfac952160217f22a270dc
Helen Song <helenhsong@gmail.com>
2026-06-19 08:30:49 +0900

    Edit about info

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
         <p>Product Designer in New York</p>
       </header>
       <section className="about">
-        <p>Previously shaped foundational product experiences for institutions at Coinbase. I enjoy simplifying complex systems and designing for scale, with a focus on building well-crafted interfaces.</p>
-        <p>Currently on sabbatical, spending part of my time learning things I've been putting off and working on side projects. Open to new opportunities in fall 2026.</p>
+        <p>Previously shaped foundational product experiences for institutions at Coinbase. I enjoy simplifying complex systems, designing for scale, and crafting polished interfaces.</p>
+        <p>Now on sabbatical, spending part of my time learning things I've been putting off and working on side projects. Open to new opportunities starting fall 2026.</p>
         <p>You can find me on <a href="https://linkedin.com/in/helenhsong" target="_blank">LinkedIn</a><span className="linkArrow"><Arrow fill="var(--gray2)" /></span> or reach out via <a href="mailto:helenhsong@gmail.com">email</a><span className="linkArrow"><Arrow fill="var(--gray2)" /></span>.</p>
       </section>
       <section className="projectList">
