c793129d2189b4a840cba88f4d7504a069dcf00b
Helen Song <helenhsong@gmail.com>
2026-09-18 23:24:16 +0900

    Add Cursor Playground project to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -36,6 +36,11 @@ const work: WorkItem[] = [
 
 const projects: WorkItem[] = [
   {
+    title: 'Cursor Playground',
+    description: 'Tiny experiments in pointer play',
+    href: 'https://helenhsong.com/cursor-playground',
+  },
+  {
     title: 'Cyworld',
     description: '2000s-era profile page',
     href: '/cyworld/',
